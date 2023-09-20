dea5003e3a8cfee61d4691bbff434475a3f9a4e0
Steve Brown <stbrown@6browns.org>
2023-09-20 14:25:34 -0400

    new bike post

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,6 +3,6 @@ module.exports = {
   title: 'BFNet Blog', // Title for the site. This will be displayed in the navbar.
   theme: '@vuepress/theme-blog',
   themeConfig: {
-    logo: '/images/Gartoon-network-48.png'
+    logo: '/Cannonball/Gartoon-network-48.png'
   }
 }
